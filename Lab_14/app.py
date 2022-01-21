3b6eff032a55a623f361d65024784c5c99cd4675
Adrian Poniatowski <adrianponiat@student.agh.edu.pl>
2022-01-21 20:22:58 +0100

    Complete Lab_14

---
+++ Lab_14/app.py
@@ -0,0 +1,28 @@
+from operator import le
+from textblob import TextBlob
+
+
+def hello(name):
+    output = f'Hello {name}'
+    return output
+
+
+def extract_sentiment(text):
+    text = TextBlob(text)
+
+    return text.sentiment.polarity
+
+
+def text_contain_word(word: str, text: str):
+    return word in text
+
+
+def bubble_sort(arr: list):
+
+    for i in range(len(arr)):
+
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
